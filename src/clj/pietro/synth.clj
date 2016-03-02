208e10f108a654fb0dae09eebd970e8e164db52b
Kyle Patterson <patterkyle@gmail.com>
2016-03-02 18:38:03 +0000

    sin and saw functions implemented

--- src/clj/pietro/synth.clj
+++ src/clj/pietro/synth.clj
@@ -11,14 +11,28 @@
 
 (def running-ugens (atom #{}))
 
-(defn play-freq [freq]
-  (let [osc (SineOscillator. freq)]
-    (swap! running-ugens conj {:freq freq :osc osc})
+(defn play-freq
+  [osc-type freq ampl]
+  (let [osc (case osc-type
+              :sin (SineOscillator.)
+              :saw (SawtoothOscillatorBL.))]
+    (.set (.frequency osc) freq)
+    (.set (.amplitude osc) ampl)
     (.add synth osc)
-    (.connect (.output osc) 0 (.input line-out) 0)))
+    (.connect (.output osc) 0 (.input line-out) 0)
+    (.connect (.output osc) 0 (.input line-out) 1)
+    (swap! running-ugens conj {:freq freq :osc-type osc-type :osc osc})))
 
-(defn stop-freq [freq]
-  (let [osc (:osc (first (filter #(= (:freq %) freq) @running-ugens)))]
-    (.disconnect (.input line-out) (.output osc))
-    (.remove synth osc)
-    (swap! running-ugens disj {:freq freq :osc osc})))
+(defn stop-freq [osc-type freq]
+  (let [osc (:osc (first (filter #(and (= (:freq %) freq)
+                                       (= (:osc-type %) osc-type))
+                                 @running-ugens)))]
+    (if osc
+      (do (.disconnect (.output osc) 0 (.input line-out) 0)
+          (.disconnect (.output osc) 0 (.input line-out) 1)
+          (.remove synth osc)
+          (swap! running-ugens disj {:freq freq :osc-type osc-type :osc osc})))))
+
+(defn stop-all []
+  (doseq [ugen @running-ugens]
+    (stop-freq (:osc-type ugen) (:freq ugen))))
